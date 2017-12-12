29b2ec6f4a8f3942521d288d53aaf5774ea6b55e
PhyrexAlianza <janwillemvanbremen@live.nl>
2017-12-12 03:10:08 +0100

    Fixed email subject

--- pages/wachtwoord_vergeten.php
+++ pages/wachtwoord_vergeten.php
@@ -58,7 +58,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
                 $to  = $email;
 
-                $subject = 'Wachtwoord-Reset';
+                $subject = 'TimeWizard - Wachtwoord-Reset';
 
                 $message = "
 <html>
